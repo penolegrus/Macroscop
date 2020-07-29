0b77826f48f751e23e35f07c6f5840b254108afb
n0icar228 <epickonfetka@gmail.com>
2020-07-29 19:27:33 +0500

    Тестовое задание BackEnd
    v1

---
+++ HTTPServerClient/client.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPServerClient
+{
+
+    class client
+    {
+        public static List<string> words = new List<string>();
+        public static string[] files = Directory.GetFiles("input", "*.txt", SearchOption.AllDirectories); //получаем файлы из папки input у клиента
+
+        const int port = 8888;
+        const string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //createlist(); функция которая покажет все текстовые файлы и их содержимое
+            Console.Write("Введите свое имя:");
+            
+            string userName = Console.ReadLine();
+            Console.WriteLine("");
+            TcpClient client = null;
+            //принимаем клиента
+            while (true)
+            {
+                try
+                {
+                    client = new TcpClient(address, port);
+                    NetworkStream stream = client.GetStream();
+
+                    while (true)
+                    {
+                        //читаем содержимое текстовых файлов из папки
+                        for (int i = 0; i < files.Length; i++)
+                        {
+                            using (StreamReader sr = new StreamReader(files[i], System.Text.Encoding.Default))
+                            {
+                                string line;
+                                while ((line = sr.ReadLine()) != null)
+                                {                                    
+                                    Console.Write(userName + ": ");
+                                    // ввод сообщения
+                                    //string message = Console.ReadLine();
+                                    string message = line;
+                                    Console.Write("текст '" + line + "' из файла " + "'" + files[i] + "'" + "\n");                                    
+                                    message = String.Format("{0}: {1}", userName, message);
+                                    // преобразуем сообщение в массив байтов
+                                    byte[] data = Encoding.Unicode.GetBytes(message);
+                                    // отправка сообщения
+                                    stream.Write(data, 0, data.Length);
+
+
+                                    // получаем ответ
+                                    data = new byte[64]; // буфер для получаемых данных
+
+                                    StringBuilder builder = new StringBuilder();
+                                    int bytes = 0;
+
+                                    do
+                                    {
+                                        bytes = stream.Read(data, 0, data.Length);
+                                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                                    }
+                                    while (stream.DataAvailable);
+
+                                    message = builder.ToString();
+                                    Console.WriteLine("Сервер: {0}", message);  
+                                }
+                            }
+                           
+                            
+                            
+                        }
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы отправить запросы еще раз\n");
+                        Console.ReadKey();
+
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+        public static void createlist()
+        {
+            //показать файлы в дериктории
+            Console.WriteLine("Файлы в директории");
+            string[] files = Directory.GetFiles("input", "*.txt", SearchOption.AllDirectories);
+            foreach (string dir in files)
+            {
+                Console.WriteLine(dir);
+            }
+           
+
+            //чтение текстовых файлов и запись в список
+            for (int i = 0; i < files.Length; i++)
+            {
+                using (StreamReader sr = new StreamReader(files[i], System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //  Console.WriteLine(line);
+                        words.Add(line);
+                     
+                    }
+                }
+            }
+            Console.WriteLine("\nСписок на экран\n");
+            foreach (string w in words)
+            {
+                Console.WriteLine(w);
+            }
+            Console.WriteLine("");
+        }
+    }
+
+}
---
+++ Server/server.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    
+    public class ClientObject
+    {       
+        public TcpClient client;
+        //создаем нового клиента
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+        //функция для проверки строки на палиндром, игнорируя пробелы и знаки препинания
+        public static bool Palindrom(string s)
+        {
+            int i = 0, j = s.Length - 1;
+            while (i < j)
+            {
+                if (char.IsWhiteSpace(s[i]) || char.IsPunctuation(s[i]))
+                    i++;
+                else if (char.IsWhiteSpace(s[j]) || char.IsPunctuation(s[j]))
+                    j--;
+                else if (char.ToLowerInvariant(s[i++]) != char.ToLowerInvariant(s[j--]))
+                    return false;
+            }
+            return true;
+        }
+        public static int _counter;
+        public static int zapros;
+
+        //асинхронная функция которая каждую секунду обнуляет счетчик запросов
+        public static async Task reset()
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                _counter = 0;
+            }
+        }
+        public static async Task mai()
+        {
+            await Task.Run(() => reset());
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(.1));
+
+                Console.WriteLine(_counter++);
+            }
+        }
+
+
+
+        public void Process()
+        {           
+             mai();
+            NetworkStream stream = null;            
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[1024]; // буфер для получаемых данных
+                while (true)
+                {
+                    
+                    _counter++; //счетчик запросов
+                    if(_counter>zapros)
+                    {
+                        Console.WriteLine("Превышено количетсво обрабатываемых запросов: {0} запросов, допустимо {1} запросов", _counter,zapros);
+                        Thread.Sleep(2000);
+                       // Console.ReadKey();
+                        break;
+                    }
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes)); //превратить байты в текст
+                    }
+                    while (stream.DataAvailable);
+                    
+                    string message = builder.ToString();
+                    //так как сообщение приходит в формате имя: текст, берем только текстовую часть
+                    string pattern = @"(\s.+$)";                   
+                    Regex regex = new Regex(pattern);
+                    MatchCollection match = regex.Matches(message);
+                    string done = Regex.Replace(match[0].Value, @"^.", ""); 
+
+                    Console.WriteLine(message);
+                    //проверям текстовую часть на палиндром
+                    if (Palindrom(done) == true)
+                    {
+                        message = "сообщение " + "'" + done + "'" + " является палиндром";
+                    }
+                    else
+                    {
+                        message = "сообщение " + "'" + done + "'" + " не является палиндром";
+                    }
+                    //отправляем ответ клиенту
+                    Thread.Sleep(server.time);
+                    data = Encoding.Unicode.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Клиент отключился");
+                //Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }  
+    }
+
+    class server
+    {
+        const int port = 8888;
+        static TcpListener listener;
+        public static int time;
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите макс. количество одновременных обрабатываемых запросов: ");
+            ClientObject.zapros = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите время обработки запроса в милисекундах(1 сек = 1000 мсек): ");            
+            time = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    //ждем пока подключится клиент
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+        
+    }
+}
